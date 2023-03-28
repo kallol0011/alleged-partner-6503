cc1fb8ae6c7fd9824d501e58e4e3060cec8cac49
kallol0011 <kallol007sarkar@gmail.com>
2023-03-28 14:40:30 +0530

    first commit

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            pull this and start working
+            
+        </div>
+    );
+};
+
+export default Home;
