2761d037cec2e85f2e3896d84c805a41766a1b50
Gourav7777 <360inside360@gmail.com>
2023-03-29 14:59:54 +0530

    asd

--- Frontend/src/Routes/AllRoutes.jsx
+++ Frontend/src/Routes/AllRoutes.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../Pages/Home';
+import Login from '../Pages/Login';
+import Signup from '../Pages/Signup';
 
 const AllRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home/>} />
-            
+            <Route path="/signup" element={<Signup></Signup>} />
+            <Route path="/login" element={<Login></Login>} />
         </Routes>
     );
 };
